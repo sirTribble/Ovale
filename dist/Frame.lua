7fe22d23e3d5ad6369a05597110d0755415d157e
sirTribble <22717586+sirTribble@users.noreply.github.com>
2018-07-23 18:02:34 +0300

    fixes

--- dist/Frame.lua
+++ dist/Frame.lua
@@ -119,6 +119,7 @@ local OvaleFrame = __class(AceGUI.WidgetContainerBase, {
         end
     end,
     OnUpdate = function(self, elapsed)
+<<<<<<< HEAD
         local guid = OvaleGUID:UnitGUID("target") or OvaleGUID:UnitGUID("focus") or OvaleGUID:UnitGUID("mouseover") or Ovale.db.profile.apparence.fullAuraScan
 =======
 >>>>>>> upstream/master
